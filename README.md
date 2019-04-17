6c9d876ef05930d6f749fe2aff9093e3931cbda0
Edward Kim <taesup63@gmail.com>
2019-04-16 14:54:00 -1000

    Update testing harness to mocha/chai

--- README.md
+++ README.md
@@ -22,10 +22,5 @@ There are **2** tests, make the first test pass.
 Once the first test passes, change the second test so that it's true,
   fill in the actual correct values in the assertions.
 
-(you'll need jasmine-node)
-
-    npm install jasmine-node -g
-
-run automated tests by running
-
-    jasmine-node --autotest .
+Running Tests:
+`npm test`
---
+++ test/spec.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+
+const SumOfASquare = require("../sumSquareDifference").SumOfASquare;
+
+describe("Difference between sum of squares and squares of sum", function () {
+
+  it("for the first 10 natural numbers is 2640", function () {
+    const squareSummer = new SumOfASquare(10);
+    expect( squareSummer.sumOfSquares() ).equal(385);
+    expect( squareSummer.squareOfTheSums() ).equal(3025);
+    expect( squareSummer.squareOfTheSums() - squareSummer.sumOfSquares() ).equal(2640);
+  });
+
+  it("for the first 100 natural numbers is XXX", function () {
+    const squareSummer = new SumOfASquare(100);
+    expect( squareSummer.sumOfSquares() ).equal(338350);
+    expect( squareSummer.squareOfTheSums() ).equal(25502500);
+    expect( squareSummer.squareOfTheSums() - squareSummer.sumOfSquares() ).equal(25164150);
+  });
+
+});
